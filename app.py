baab545abe74680f05c7d91fa34263a1bdffe43a
Wei-Ju Chen <wxc276@student.bham.ac.uk>
2024-11-11 17:52:16 +0800

    create duration function

--- app.py
+++ app.py
@@ -8,9 +8,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import datetime, timezone, timedelta
 
+import matplotlib.dates as mdates
+
 # D:\D_Download\zap_database
 
 conn = sqlite3.connect("zap_database")
+# conn = sqlite3.connect("D:\D_Download\zap_database")
 cursor = conn.cursor()
 
 
@@ -19,10 +22,7 @@ def query_info():
     query = "SELECT pm25,temperature,humidity,create_at FROM airqualitydata"
     cursor.execute(query)
     data = cursor.fetchall()
-    print(data)
-    # for row in data:
-    #     print(row)
-    # return data
+    # print(data)
 
     with open("mj.csv", "w", newline="", encoding="utf-8") as f:
         writer = csv.writer(f)
@@ -31,34 +31,46 @@ def query_info():
     print("匯出成功！")
 
 
-def plot_bar_chart():
-    db_data = pd.read_csv("mj.csv")
+interval = "5T"
+time = 5
 
+
+def plot_bar_chart(interval, time):
+    db_data = pd.read_csv("mj.csv")
+    # db_data = pd.read_csv("D:\\Work Area\\FINAL\\task1\\mj.csv")
     df = pd.DataFrame(db_data)
     df["create_at"] = pd.to_datetime(df["create_at"], unit="ms")
-    df["create_at"] = df["create_at"].dt.strftime("%Y-%m-%d %H %M")
-    # df["create_at"] = pd.to_datetime(df["create_at"].astype(int), unit="s")
-    # df["create_at"] = pd.to_datetime(df["create_at"].astype(str), format="%Y%m%d%H%M%S")
-    # df["create_at"] = pd.to_datetime(df["create_at"], unit="s")
-    # df["create_at"] = pd.to_datetime(df["create_at"], format="%Y%m%d%H%M%S")
+    df.set_index("create_at", inplace=True)
 
-    X = list(df.iloc[:, 3])
-    Y = list(df.iloc[:, 1])
+    print("Missing values before resampling:", df.isnull().sum())
 
-    # Plot the data using bar() method
+    df_resampled = df.resample(interval).mean().reset_index()
+    df_resampled = df_resampled.fillna(method="ffill")
+    # print("Here is df_resampled", df_resampled)
+
+    X = list(df_resampled["create_at"])
+    Y = list(df_resampled["temperature"])
+
     plt.bar(X, Y, color="g")
     plt.title("Temperature over time")
     plt.xlabel("Time")
     plt.ylabel("Temperature")
-    plt.xticks(rotation=45)
 
+    plt.gca().xaxis.set_major_locator(mdates.MinuteLocator(interval=time))
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d %H:%M"))
+
+    plt.xticks(rotation=45)  # 旋轉 X 軸標籤
+
+    # 儲存為 JPG 圖檔
     plt.tight_layout()
-    plt.savefig(
-        "bar_chart.jpg", format="jpg", dpi=300
-    ) 
+    plt.savefig("bar_chart.jpg", format="jpg", dpi=300)
     plt.show()
+    print("圖表已經成功儲存！")
 
 
+# plot_bar_chart("15T", 15)
+# plot_bar_chart("30T", 30)
+# plot_bar_chart("60T", 60)
 
 root = tk.Tk()
 root.title("GUI Ver1 @v0.02 2024-11-11")
@@ -86,7 +98,7 @@ button1_2 = tk.Button(
     tab1,
     text="Step 2",
     font=("Arial", 16),
-    command=plot_bar_chart,
+    command=lambda: plot_bar_chart("5T", 5),
     # command=lambda: print("Page 1 按鈕 2 被點擊"),
 )
 button1_2.grid(column=1, row=0, padx=10, pady=10)
@@ -122,8 +134,9 @@ button1_6 = tk.Button(
     tab1,
     text="15 minutes",
     font=("Arial", 16),
+    command=lambda: plot_bar_chart("15T", 15),
     # command=plot_bar_chart(15),
-    command=lambda: print("間隔時間15分鐘"),
+    # command=lambda: print("間隔時間15分鐘"),
 )
 button1_6.grid(column=0, row=10, padx=10, pady=10)
 
@@ -131,7 +144,8 @@ button1_7 = tk.Button(
     tab1,
     text="30 minutes",
     font=("Arial", 16),
-    command=lambda: print("間隔時間30分鐘"),
+    command=lambda: plot_bar_chart("30T", 30),
+    # command=lambda: print("間隔時間30分鐘"),
 )
 button1_7.grid(column=1, row=10, padx=10, pady=10)
 
@@ -139,7 +153,8 @@ button1_8 = tk.Button(
     tab1,
     text="60 minutes",
     font=("Arial", 16),
-    command=lambda: print("間隔時間60分鐘"),
+    command=lambda: plot_bar_chart("60T", 60),
+    # command=lambda: print("間隔時間60分鐘"),
 )
 button1_8.grid(column=2, row=10, padx=10, pady=10)
 
